31c55b2165be3dca0adef958c6dd277fbc6e7f3e
Phil Wright <phil.wright@componentfactory.com>
2025-12-06 17:30:11 +1100

    NewConnection

--- src/Sharp-O-Matic.Editor/src/app/pages/connections/connections.component.ts
+++ src/Sharp-O-Matic.Editor/src/app/pages/connections/connections.component.ts
@@ -1,27 +1,30 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { ServerRepositoryService } from '../../services/server.repository.service';
 import { ConnectionSummary } from '../../metadata/definitions/connection summary';
 import { ConfirmDialogComponent } from '../../dialogs/confirm/confirm-dialog.component';
+import { SelectConnectorDialogComponent } from '../../dialogs/new-connector/new-connector-dialog.component';
 import { Connection } from '../../metadata/definitions/connection';
 
 @Component({
   selector: 'app-connections',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: './connections.component.html',
   styleUrls: ['./connections.component.scss'],
-    providers: [BsModalService]
+  providers: [BsModalService]
 })
 export class ConnectionsComponent {
   private readonly serverWorkflow = inject(ServerRepositoryService);  
   private readonly modalService = inject(BsModalService);
   private readonly router = inject(Router);  
-  private bsModalRef: BsModalRef<ConfirmDialogComponent> | undefined;
+  private confirmModalRef: BsModalRef<ConfirmDialogComponent> | undefined;
+  private selectConnectorModalRef: BsModalRef<SelectConnectorDialogComponent> | undefined;
   
   public connections: ConnectionSummary[] = [];
 
@@ -32,27 +35,38 @@ export class ConnectionsComponent {
   }
 
   newConnection(): void {
-    // TODO, get user to choose from connection config options
-    const newConnection = new Connection({
-      ...Connection.defaultSnapshot(),
-      name: 'Untitled',
-      description: 'New connection needs a description.',
-    });
-    this.serverWorkflow.upsertConnection(newConnection).subscribe(() => {
-      this.router.navigate(['/connections', newConnection.connectionId]);
+    this.selectConnectorModalRef = this.modalService.show(SelectConnectorDialogComponent);
+
+    this.selectConnectorModalRef.onHidden?.subscribe(() => {
+      const selectedConfigId = this.selectConnectorModalRef?.content?.resultConfigId;
+
+      if (!selectedConfigId) {
+        return;
+      }
+
+      const newConnection = new Connection({
+        ...Connection.defaultSnapshot(),
+        configId: selectedConfigId,
+        name: 'Untitled',
+        description: 'Connection needs a description.',
+      });
+
+      this.serverWorkflow.upsertConnection(newConnection).subscribe(() => {
+        this.router.navigate(['/connections', newConnection.connectionId]);
+      });
     });
   }
 
   deleteConnection(connection: ConnectionSummary) {
-    this.bsModalRef = this.modalService.show(ConfirmDialogComponent, {
+    this.confirmModalRef = this.modalService.show(ConfirmDialogComponent, {
       initialState: {
         title: 'Delete Connection',
         message: `Are you sure you want to delete the connection '${connection.name}'?`
       }
     });
 
-    this.bsModalRef.onHidden?.subscribe(() => {
-      if (this.bsModalRef?.content?.result) {
+    this.confirmModalRef.onHidden?.subscribe(() => {
+      if (this.confirmModalRef?.content?.result) {
         this.serverWorkflow.deleteConnection(connection.connectionId).subscribe(() => {
           this.connections = this.connections.filter(c => c.connectionId !== connection.connectionId);
         });
